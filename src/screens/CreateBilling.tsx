5a29178fb501b508f9dff2ecbdd2ee7cd0d98b70
dhanushqtechx003 <qtechx003@gmail.com>
2026-03-23 10:41:41 +0530

    Voice command

--- src/screens/CreateBilling.tsx
+++ src/screens/CreateBilling.tsx
@@ -10,7 +10,7 @@ import Feather from 'react-native-vector-icons/Feather';
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "../contexts/AuthContext";
 import { fetchProducts, fetchCategories, createBill, Product, Category, BillPayload } from "../api";
-import Voice from '@react-native-voice/voice';
+// import Voice from '@react-native-voice/voice';
 import { useRoute } from "@react-navigation/native";
 
 const { width } = Dimensions.get('window');
